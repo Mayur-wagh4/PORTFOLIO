a729abae3ca4a6c309e2b7fd1e80071947b4e7c5
vaishnavi dhokare <mayurwagh2001@gmail.com>
2025-01-19 23:22:25 +0530

    bgupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Hero, Navbar, Skill, Tech, Works } from "./components";
 import StarsCanvas from "./components/canvas/Stars";
 import Certifications from "./components/certifications";
 
 function App() {
+  const [starsLoaded, setStarsLoaded] = useState(false);
+
+  const handleStarsLoaded = () => {
+    setStarsLoaded(true);
+  };
+
   return (
     <BrowserRouter>
       <div className="relative">
         {/* Stars Canvas as Background */}
-        <div className="absolute inset-0 -z-10">
-          <StarsCanvas />
+        <div
+          className={`absolute inset-0 -z-10 transition-colors duration-500 ${
+            starsLoaded ? "bg-transparent" : "bg-black"
+          }`}
+        >
+          <StarsCanvas onLoad={handleStarsLoaded} />
         </div>
 
         {/* App Content */}
         <Navbar />
         <Hero />
-        
+
         <main className="relative z-10">
           {/* About Section */}
           <About />
